bc29a8c166372b7a6eafe47135a32cda00c9091c
standaloneee <tzherzdev@gmail.com>
2022-07-05 18:53:42 +0300

    init commit

---
+++ src/main/java/ru/standalone/films_catalog/FilmsCatalogApplication.java
@@ -0,0 +1,13 @@
+package ru.standalone.films_catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmsCatalogApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FilmsCatalogApplication.class, args);
+  }
+
+}
---
+++ src/test/java/ru/standalone/films_catalog/FilmsCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.standalone.films_catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmsCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
